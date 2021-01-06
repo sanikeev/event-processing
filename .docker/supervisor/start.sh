85ce6713a38ed351459c6eb1f6b94015f30747d6
sanikeev <keshbuk@gmail.com>
2021-01-07 02:56:45 +0300

    fix config

---
+++ .docker/supervisor/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+FILE=/var/www/symfony/vendor/autoload.php
+
+cmd="$@"
+
+>&2 echo "!!!!!!!! Check autoload.php for available !!!!!!!!"
+
+until [ ! -f "$FILE" ]; do
+  >&2 echo "Autoload.php is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Autoload.php is up - executing command"
+
+exec $cmd
+
